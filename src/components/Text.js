3fbf0076aa485a6c08118fcc47d6951faf53b1d6
Teodora Husar <husarteodora@yahoo.com>
2020-11-14 11:12:51 +0200

    fixes

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import './Text.scss'
 export default function Text({input}) {
     return (
-        <p>
+        <p className="text">
             {input}
         </p>
     )
--- src/data/lessons.js
+++ src/data/lessons.js
@@ -1,33 +1,30 @@
 export default [
   {
     lessonId: 1,
-    title: "ZOOM intro",
+    title: "Introducere in Zoom",
     lessons: [
       {
-        title: "intro 1",
+        title: "Cum intrii in ședințe Zoom",
         content: [
           {
             type: "text",
-            input: "Primul text",
+            input: "Pentru a te alătura unei ședințe Zoom, trebuie să îi știi ID-ul și parola, pe care le primești de la gazda conferinței. Gazda unei ședințe Zoom îți poate trimite ID-ul și parola prin numeroase metode, inclusiv, dar nu limitat la e-mail, chat, mesaje sau invitații directe în aplicația Zoom. Indiferent cum primești invitația, ar trebui să ai un link sau un ID și o parolă pentru ședința Zoom. De exemplu, dacă primești invitația prin e-mail, ea arată astfel",
           },
           {
             nr: 2,
             type: "image",
-            input: "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search-v2_297x176.jpg",
+            input: "/images/zoom_11.png",
           },
         ],
       },
       {
-        title: "intro 1",
+        title: "",
         content: [
           {
             type: "text",
-            input: "Al doilea slide",
+            input: "Cel de-al doilea lucru pe care trebuie să îl ai în vedere este faptul că poți să intri într-o conferință Zoom fără a te conecta la contul tău de Zoom sau chiar fără să ai un cont Zoom. Poți face acest lucru din aplicația Zoom pe PC-ul sau dispozitivul tău mobil, dar trebuie mai întâi să instalezi aplicația. Este posibil să intri într-o conferință Zoom și dintr-un browser web, fără a instala aplicația, dar trebuie să ai un cont Zoom pentru a o putea face. Dacă ai nevoie de ajutor pentru instalarea Zoom pe PC, smartphone sau tabletă, ori pentru crearea unui cont Zoom",
           },
-          {
-            type: "text",
-            input: "Slide 2",
-          },
+        
         ],
       },
       {
@@ -35,7 +32,7 @@ export default [
         content: [
           {
             type: "text",
-            input: "Al treilea slide",
+            input: "De asemenea, trebuie să știi că Zoom nu este disponibil în limba română, așa că pentru acest tutorial folosim Zoom în limba engleză, dar adăugăm traduceri pentru fiecare opțiune prezentată.",
           },
           
         ],
